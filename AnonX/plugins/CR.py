44771737a22be9c264a4f18d7975fc0dd8a71be1
maro <g551552g@gamil.com>
2023-12-23 05:15:14 +0300

    first commit

---
+++ AnonX/plugins/CR.py
@@ -0,0 +1,195 @@
+import asyncio
+
+import os
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import app
+from random import  choice, randint
+
+                
+@app.on_message(
+    command(["مطورين البوب","المطورين","مطورين","مطورين "])
+    & ~filters.edited
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/c973727bddf803e028539.jpg",
+        caption=f"""**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**\nمرحبا بك عزيزي {message.from_user.mention} في قسم مطورين البوب ميوزك\nللتحدث مع مطورين اضغط علي الازرار بالاسفل👇\n**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝑀𝐴𝑅𝑂 ", url=f"https://t.me/j_s_9"), 
+                 ],[
+                    InlineKeyboardButton(
+                        "𝐴𝐿𝑃𝑂𝑃", url=f"https://t.me/vip_alpop"),
+                ],[
+                    InlineKeyboardButton(
+                        "𝐶𝑂𝑀𝑀𝐔𝑁𝐼𝐶𝐴𝑇𝐼𝑂𝑁", url=f"https://t.me/O_U_O_BOT"),
+                    InlineKeyboardButton(
+                        "𝐺𝑅𝑂𝐔𝑃", url=f"https://t.me/bar_alpop"),
+                ],[
+                
+                    InlineKeyboardButton(
+                        "𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊", url=f"https://t.me/source_alpop"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+
+
+
+
+
+
+
+@app.on_message(
+    command(["ماروو","مارووو","الملك","مبرمج","maro","مارو"])
+    & filters.group
+    & ~filters.edited
+)
+async def yas(client, message):
+    usr = await client.get_chat("j_s_9")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺\n\n‍ ¦namee :{name}\n ¦u𝘴e𝘳 :@{usr.username}\n ¦Ꭵժ :`{usr.id}`\n ¦ႦᎥo :{usr.bio}\n\n**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+
+
+@app.on_message(
+    command(["البوب","مطور","امير","elpop","alpop"])
+    & filters.group
+    & ~filters.edited
+)
+async def yas(client, message):
+    usr = await client.get_chat("vip_alpop")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺\n\n¦namee :{name}\n ¦u𝘴e𝘳 :@{usr.username}\n ¦Ꭵժ :`{usr.id}`\n ¦ႦᎥo :{usr.bio}\n\n**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+
+
+@app.on_message(
+    command(["التسليه"])
+    & filters.group
+    & ~filters.edited
+)
+async def yas(client, message):
+    usr = await client.get_chat("j_s_9")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺\n\n‍ ★¦ اوامر التسليه ( الرفع )
+★¦ رفع نمله+تنزيل نمله
+★¦ رفع صرصار+تنزيل صرصار 
+★¦ رفع رقاصه+تنزيل رقاصه 
+★¦ رفع متناك+تنزيل متناك 
+★¦ رفع نجس+تنزيل نجس 
+★¦رفع عره+تنزيل عره
+★¦ رفع بقره+تنزيل بقره
+★¦ رفع قرد+تنزيل قرد
+★¦ رفع قلبي+تنزيل قلبي 
+★¦رفع خدام+تنزيل خدام
+★¦ رفع عرص+تنزيل عرص
+★¦ رفع ارمله+تنزيل ارمله
+★¦ رفع مزه+تنزيل مزه
+★¦رفع ابني+تنزيل ابني 
+★¦ رفع خاينه+تنزيل خاينه
+★¦ رفع بنتي+تنزيل بنتي
+★¦ رفع خاين+تنزيل خاين
+★¦ رفع خول+تنزيل خول
+★¦ رفع حمار+تنزيل حمار 
+★¦ رفع غبي+تنزيل غبي 
+★¦ رفع مراتي+تنزيل مراتي 
+★¦ رفع زبال+تنزيل زبال
+★¦ رفع خدامه+تنزيل خدامه
+★¦ رفع كلبه+تنزيل كلبه
+★¦ رفع طيز+تنزيل طيز 
+★¦ رفع حرامي+تنزيل حرامي \n\n**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+    
+
+@app.on_message(
+    command(["هديه"])
+    & filters.group
+    & ~filters.edited
+)
+async def yas(client, message):
+    usr = await client.get_chat("C1_I_I")
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺\n\n دي بقا يباشا الاكس الكسرت قلبي الله يرقها يارب\n\n**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{usr.username}")
+                ],
+            ]
+        ),
+    )
+    
+
+
+@app.on_message(
+    command(["/api"])
+    & ~filters.edited
+    
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/c973727bddf803e028539.jpg",
+        caption=f"""**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**\nمرحبا بك عزيزي {message.from_user.mention} في قسم الذكاء الاصتناعي الخاص بسورس cr\nلتتمكن من استخدام اوامر الذكاء الاصتناعي اكتب \n /gpt + السؤال بالاسفل👇\n**⩹━⊷⌯⌞ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ⌝⌯⊶━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝐴𝐿𝑃𝑂𝑃", url=f"https://t.me/vip_alpop"), 
+                 ],[
+                
+                    InlineKeyboardButton(
+                        "𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊", url=f"https://t.me/source_alpop"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+
+
---
+++ AnonX/plugins/sorse.py
@@ -0,0 +1,61 @@
+import asyncio
+
+import os
+import config
+import time
+import requests
+from config import START_IMG_URL
+from pyrogram import filters
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from strings.filters import command
+from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AnonX import app
+from random import  choice, randint
+@app.on_message(
+    command(["سورس مين","سورس","السورس","سورس مارو", "سورس البوب"])
+    & ~filters.edited
+)
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph//file/8a417ee5ee410523952b3.jpg",
+        caption=f"""𝘛𝘏𝘌 𝘉𝘌𝘚𝘛 𝘚𝘖𝘜𝘙𝘊𝘌 𝘖𝘕 𝘛𝘌𝘓𝘌𝘎𝘙𝘈𝘔""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝑀𝐴𝑅𝑂", url=f"https://t.me/j_s_9"), 
+                
+                    InlineKeyboardButton(
+                        "𝐴𝐿𝑃𝑂𝑃", url=f"https://t.me/vip_alpop"),
+                ],[
+                    InlineKeyboardButton(
+                        " 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ", url=f"https://t.me/source_alpop"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+
+
+@app.on_message(command(["غنيلي", "غني", "غ", "🎙 ¦ غـنيـلي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(3,267)
+    url = f"https://t.me/bsmaatt/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاغـنـية لـك",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
+
+
